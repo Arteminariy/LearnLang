8275bd41cd4631655075f74d2f91f1a5deb506b1
Arteminariy <artemborisoff.ab@gmail.com>
2023-04-30 21:39:19 +0300

    upd: Layout > add: header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import NavBar, { INavBarLink } from '../NavBar/NavBar';
+import styles from './Header.module.scss'
 
 const Header: FC = () => {
-	return <div>Header</div>;
+
+	
+	const navBarLinks: INavBarLink[] = [
+		{href: '/', text: 'Главная'},
+		{href: '/language', text: 'Языки'}
+	]
+
+	return (
+	<header className={styles.header}>
+		<NavBar navBarLinks={navBarLinks}/>
+	</header>
+	);
 };
 
 export default Header;
---
+++ client/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import styles from './NavBar.module.scss';
+import Link from 'next/link';
+
+export type INavBarLink = {
+	href: string;
+	text: string;
+};
+
+type INavBarProps = {
+	navBarLinks: INavBarLink[];
+};
+
+const NavBar: FC<INavBarProps> = ({ navBarLinks }) => {
+	return (
+		<nav className={styles.navigation}>
+			<div className={styles.navigation__links}>
+				{navBarLinks.map((el) => {
+					return (
+						<Link
+							key={el.text}
+							href={el.href}
+							className={styles.navigation__links__link}
+						>
+							{el.text}
+						</Link>
+					);
+				})}
+			</div>
+		</nav>
+	);
+};
+
+export default NavBar;
