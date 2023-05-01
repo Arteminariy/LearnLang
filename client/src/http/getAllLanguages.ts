7343a9d0ff8f6977dee22cc608b691235899743b
Arteminariy <artemborisoff.ab@gmail.com>
2023-05-01 04:18:13 +0300

    fix

--- client/src/http/getAllLanguages.ts
+++ client/src/http/getAllLanguages.ts
@@ -2,6 +2,6 @@ import { ILanguage } from "@/@types"
 import axios from "axios"
 
 export const getAllLanguages = async () => {
-    const res = await axios.get<ILanguage[]>(`${process.env.NEXT_PUBLIC_SERVER_API}/languages/` || `http://localhost:4200/languages/`)
+    const res = await axios.get<ILanguage[]>(`${process.env.NEXT_PUBLIC_SERVER_API}/languages/`)
     return res.data
 }
